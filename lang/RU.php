2e2120d9ac621c5ffe4c8bb88f8476c49e7d3c8b
tabr <tabr@xaker.ru>
2011-03-24 12:59:28 +0200

    UI: expires can be selected by calendar, added language support, moved files (config/auc) to (config/kernel) dirs

---
+++ lang/RU.php
@@ -0,0 +1,12 @@
+<?php
+define ('MSG_AUCTION_CREATE_OK',	'аукцион создан');
+define ('MSG_AUCTION_CREATE_ERROR',	'ошибка создания аукциона');
+define ('MSG_AUCTION_CANCEL_OK',	'аукцион отменен');
+define ('MSG_AUCTION_CANCEL_ERROR',	'ошибка отмены аукциона');
+define ('MSG_AUCTION_BUYOUT_OK',	'персонаж выкуплен');
+define ('MSG_AUCTION_BUYOUT_ERROR',	'ошибка выкупа персонажа');
+define ('MSG_AUCTION_BID_OK',		'ставка принята');
+define ('MSG_AUCTION_BID_ERROR',	'ошибка ставки');
+define ('MSG_TRANSACTION_START_ERROR',	'ошибка начала транзакции');
+
+?>
