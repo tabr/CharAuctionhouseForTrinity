2818deb691811078afd75e6a57be9fcdf3a05162
tabr <tabr@xaker.ru>
2011-03-24 10:45:24 +0200

    add forgotten config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define ('DB_AUC_HOST',		'localhost');
+define ('DB_AUC_USER',		'root');
+define ('DB_AUC_PASS',		'root');
+define ('DB_AUC_BASE',		'temp');
+define ('LOGS_DIR',		'log');
+define ('MAX_CHARS_ON_ACCOUNT',	10);
+?>
